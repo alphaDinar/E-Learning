7e4577bf0faa6b60fc3110bb36bace49aa7c11e4
alphaDinar <yanksone18@yahoo.com>
2023-05-05 17:28:14 +0000

    change

--- ELearning/settings.py
+++ ELearning/settings.py
@@ -66,16 +66,16 @@ WSGI_APPLICATION = 'ELearning.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.parse('postgres://e_learning_3di0_user:kFClASjxiv8fQmZNCvVT49e886uBD4kF@dpg-chae9m3hp8u791i4dc80-a.oregon-postgres.render.com/e_learning_3di0')
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.parse('postgres://e_learning_3di0_user:kFClASjxiv8fQmZNCvVT49e886uBD4kF@dpg-chae9m3hp8u791i4dc80-a.oregon-postgres.render.com/e_learning_3di0')
+}
 
 
 # Password validation
